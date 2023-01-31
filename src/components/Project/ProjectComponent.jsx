e11ac0b9eb140dd3cce700ee6c4bd397b7c7f56e
fescarvalho <fernando_scarvalho@hotmail.com>
2023-01-31 15:59:18 -0300

    :sparkels: feat - Created of page project, and component project.

---
+++ src/components/Project/ProjectComponent.jsx
@@ -0,0 +1,16 @@
+import './ProjectComponent.css';
+
+function ProjectComponent() {
+  return (
+    <div>
+      <div className="project" id="project">
+        <div className="name">
+          <h3>Nome do Projeto</h3>
+        </div>
+        <button className="button">Ver mais</button>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectComponent;
--- src/pages/Project/Projetos.jsx
+++ src/pages/Project/Projetos.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/mouse-events-have-key-events */
+
+import './Projetos.css';
 import NavButton from '../../components/NavButton/NavButton';
+import ProjectComponent from '../../components/Project/ProjectComponent';
 
 function Projetos() {
   return (
-    <div>
+    <>
       <NavButton />
-    </div>
+      <div className="content">
+        <div className="page-description">
+          <h2>Portifolio</h2>
+          <p>
+            Aqui estão alguns dos meus projetos, desenvolvidos com tecnologias
+            para desenvolvimento Web, como, JavaScript, ReactJs, Typescript,
+            SASS, NodeJs, entre outras.
+          </p>
+        </div>
+        <div className="project-main">
+          <ProjectComponent />
+          <ProjectComponent />
+          <ProjectComponent />
+          <ProjectComponent />
+          <ProjectComponent />
+          <ProjectComponent />
+        </div>
+      </div>
+    </>
   );
 }
 
