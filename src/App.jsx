6fb1f7c17ef46c9b96f58c7c3656a0cdeebadda8
fescarvalho <fernando_scarvalho@hotmail.com>
2023-02-03 00:30:59 -0300

    :sparkles: feat - Add project in routes.

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,100 @@ function App() {
         <Route path="/projetos" element={<Projetos />} />
         <Route path="/contato" element={<Contato />} />
 
-        <Route path="/projetos/dogs" element={<PageProject />} />
-        <Route path="/projetos/bike" element={<Contato />} />
-        <Route path="/projetos/supermarket" element={<Contato />} />
-        <Route path="/projetos/lescone" element={<Contato />} />
-        <Route path="/projetos/hoursbus" element={<Contato />} />
-        <Route path="/projetos/walletapp" element={<Contato />} />
+        <Route
+          path="/projetos/dogs"
+          element={
+            <PageProject
+              name="Dogs-Rede Social"
+              srcLink="https://dogs-social.vercel.app/"
+              srcGithub="https://github.com/fescarvalho/Dogs"
+              srcImage="/images/DogsSocial/image2.png"
+              description="Projeto de Rede Social para pets."
+              tecs={['React', 'CSSModules', 'MongoDb']}
+            />
+          }
+        />
+        <Route
+          path="/projetos/bike"
+          element={
+            <PageProject
+              name="Bike Craft"
+              srcLink="https://bikcraftmotors.netlify.app/"
+              srcGithub="https://github.com/fescarvalho/bikcraft"
+              srcImage="/images/BikeCraft/image1.png"
+              description="Projeto de site para compra de Bicicletas e Seguros."
+              tecs={['HTML', 'CSS', 'JavaScript']}
+            />
+          }
+        />
+        <Route
+          path="/projetos/supermarket"
+          element={
+            <PageProject
+              name="Super Market List"
+              srcLink="https://app-supermarket-list-front.vercel.app/"
+              srcGithub="https://github.com/fescarvalho/APP-SupermarketList-Front"
+              srcImage="/images/SuperMarket/image2.png"
+              description="Projeto criado para auxilio na organização de lista de compras de supermercado. Foi construida API para salvamento dos dados e cadastro do usuario."
+              tecs={[
+                'React',
+                'CSS',
+                'Axios',
+                'NodeJs',
+                'Docker',
+                'Mongoose',
+                'MongoDB',
+                'Express',
+              ]}
+            />
+          }
+        />
+        <Route
+          path="/projetos/lescone"
+          element={
+            <PageProject
+              name="Le Scone Restaurante"
+              srcLink="https://le-scone-degust.vercel.app"
+              srcGithub="https://github.com/fescarvalho/LeScone"
+              srcImage="/images/LeScone/image1.png"
+              description="Projeto criado para divulgação de Restaurante."
+              tecs={['HTML', 'SASS', 'JavaScript']}
+            />
+          }
+        />
+        <Route
+          path="/projetos/hoursbus"
+          element={
+            <PageProject
+              name="Hours Bus"
+              srcLink="https://hoursbus.vercel.app"
+              srcGithub="https://github.com/fescarvalho/HoursBus"
+              srcImage="/images/hoursBus/image1.png"
+              description="Projeto criado para divulgação de horario de onibus."
+              tecs={['Typescript', 'React', 'CssModules']}
+            />
+          }
+        />
+        <Route
+          path="/projetos/walletapp"
+          element={
+            <PageProject
+              name="Wallet App"
+              srcLink="https://wallet-app-frontend.vercel.app"
+              srcGithub="https://github.com/fescarvalho/wallet-app-frontend"
+              srcImage="/images/WalletApp/image1.png"
+              description="Projeto criado para auxilio no controle de gastos mensais."
+              tecs={[
+                'React',
+                'CSS',
+                'NodeJs',
+                'Docker',
+                'PostgresSQL',
+                'Express',
+              ]}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/PageProject/PageProject.jsx
+++ src/pages/PageProject/PageProject.jsx
@@ -9,13 +9,7 @@ function PageProject({
   srcGithub,
   srcImage,
   description,
-  tec1,
-  tec2,
-  tec3,
-  tec4,
-  tec5,
-  tec6,
-  tec7,
+  tecs,
 }) {
   return (
     <div className="projeto-main">
@@ -37,12 +31,22 @@ function PageProject({
       </div>
       <div className="contentProject">
         <div className="mainProject">
-          <h1>{name}Projeto 1</h1>
+          <h1>{name}</h1>
           <div className="links">
-            <a className="visite" href={srcLink}>
+            <a
+              className="visite"
+              href={srcLink}
+              target="_blank"
+              rel="noreferrer"
+            >
               Visite
             </a>
-            <a className="github" href={srcGithub}>
+            <a
+              className="github"
+              href={srcGithub}
+              target="_blank"
+              rel="noreferrer"
+            >
               Github
             </a>
           </div>
@@ -52,36 +56,20 @@ function PageProject({
         </div>
         <div className="description">
           <h2>Sobre o projeto</h2>
-          <p>
-            {description}
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the standard dummy text ever since
-            the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only
-            five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
+          <p>{description}</p>
         </div>
         <div className="tecs">
           <h2>Tecnologias Usadas</h2>
           <ul>
-            <i>{tec1}JavaScript</i>
-            <i>{tec2}JavaScript</i>
-            <i>{tec3}JavaScript</i>
-            <i>{tec4}JavaScript</i>
-            <i>{tec5}JavaScript</i>
-            <i>{tec6}JavaScript</i>
-            <i>{tec7}JavaScript</i>
+            {tecs.map((tec) => (
+              <i>{tec}</i>
+            ))}
           </ul>
         </div>
         <div className="acesse">
           <h2>Acesse Aqui</h2>
-          <a href={srcLink} target="_parent">
+          <a href={srcLink} target="_blanck">
             {srcLink}
-            https://edrodrigues.com.br/blog/20-incriveis-botoes-animados-de-css-puro/
           </a>
         </div>
       </div>
