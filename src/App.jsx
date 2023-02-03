617bd328ba0c506fc5c5c3fb07728ca013c98865
fescarvalho <fernando_scarvalho@hotmail.com>
2023-02-03 20:42:53 -0300

    :bug: fix - Correction of bug in the project hourbus.

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ function App() {
               name="Hours Bus"
               srcLink="https://hoursbus.vercel.app"
               srcGithub="https://github.com/fescarvalho/HoursBus"
-              srcImage="/images/hoursBus/image1.png"
+              srcImage="/images/HoursBus/image1.png"
               description="Projeto criado para divulgação de horario de onibus."
               tecs={['Typescript', 'React', 'CssModules']}
             />
