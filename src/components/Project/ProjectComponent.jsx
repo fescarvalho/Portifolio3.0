7653ac365d52e333eb27a9803ff6ee9cf85144cb
fescarvalho <fernando_scarvalho@hotmail.com>
2023-02-02 23:57:52 -0300

    :sparkles: feat - Correction of bug in espacament hte class project.

--- src/components/Project/ProjectComponent.jsx
+++ src/components/Project/ProjectComponent.jsx
@@ -3,16 +3,14 @@ import './ProjectComponent.css';
 
 function ProjectComponent({ name, src, linkImage1, linkImage2 }) {
   return (
-    <div>
-      <div className="project">
-        <div className="name">
-          <h3>{name}</h3>
-        </div>
-        <a href={src}>Ver mais</a>
-        <div className="main-image">
-          <img src={linkImage1} alt="" className="image1" />
-          <img src={linkImage2} alt="" className="image2" />
-        </div>
+    <div className="project">
+      <div className="name">
+        <h3>{name}</h3>
+      </div>
+      <a href={src}>Ver mais</a>
+      <div className="main-image">
+        <img src={linkImage1} alt="" className="image1" />
+        <img src={linkImage2} alt="" className="image2" />
       </div>
     </div>
   );
