774e4e4ab08f5b9806e69715dcd81f2e5d9c508c
fescarvalho <fernando_scarvalho@hotmail.com>
2023-03-21 21:18:01 -0300

    feat - Correção nas rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 // eslint-disable-next-line import/extensions
-import AppRoutes from './routes/app.routes';
+import AppRoutes from './routes/app.routes.jsx';
 
 function App() {
   return (
